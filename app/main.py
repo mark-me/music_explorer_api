d18ad17a7e5c610bab93eb01e58f8db72e3fedae
Mark Zwart <mark-me@users.noreply.github.com>
2022-05-23 11:05:21 +0200

    Adding sqlite connection function

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+file_db = "/data/discogs.sqlite"
 
 @app.get("/")
 def read_root():
@@ -14,3 +15,17 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
     
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
