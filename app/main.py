e7b4d2c9ebdcfdbfb5adde52913d8cac92e34cb5
Mark Zwart <mark.zwart@pobox.com>
2022-06-05 11:54:30 +0200

    Added collection artist releases view

--- app/main.py
+++ app/main.py
@@ -20,16 +20,20 @@ def read_collection_artists(
     collection_artists = _services.get_collection_artists(db=db, skip=skip, limit=limit)
     return collection_artists
 
-@app.post("/artist_collection_releases/")
-def read_artist_collection_releases(
-    skip: int=0, 
-    limit: int=999, 
+@app.post("/collection_artist_releases/{id_artist}")
+def read_collection_artist_releases(
+    id_artist: str,
+    skip: int = 0,
+    limit: int = 10,
     db: _orm.Session=_fastapi.Depends(_services.get_db),
     ):
-    collection_releases = _services.get_artist_collection_releases(db=db, skip=skip, limit=limit)
+    db_collection_artist  = _services.get_collection_artist(db=db, id_artist=id_artist)
+    if db_collection_artist is None:
+        raise _fastapi.HTTPException(
+            status_code=404, detail="Sorry, this artist doesn't exist in your collection"
+        )
+    collection_releases = _services.get_collection_artist_releases(db=db, id_artist=id_artist,skip=skip, limit=limit)
     return collection_releases
-
-
 
 
 if __name__ == "__main__":
--- app/services.py
+++ app/services.py
@@ -15,11 +15,15 @@ def get_db():
         db.close()
 
 
+def get_collection_artist(db: _orm.Session, id_artist: str):
+    return db.query(_models.CollectionArtist).filter(_models.CollectionArtist.id_artist == id_artist).first()
+
+
 def get_collection_artists(db: _orm.Session, skip:int, limit:int):
     collection_artists = db.query(_models.CollectionArtist).offset(skip).limit(limit).all()
     return collection_artists
     
 
-def get_artist_collection_releases(db: _orm.Session, skip:int, limit:int):
-    collection_releases = db.query(_models.CollectionRelease).offset(skip).limit(limit).all()
+def get_collection_artist_releases(db: _orm.Session, id_artist: str, skip:int, limit:int):
+    collection_releases = db.query(_models.CollectionRelease).filter(_models.CollectionRelease.id_artist == id_artist).offset(skip).limit(limit).all()
     return collection_releases    
