79d774fcb0fcd88c0a0795a639f9fa85f154af1b
Mark Zwart <mark.zwart@pobox.com>
2022-06-05 14:38:54 +0200

    Fix database URL

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///data/discogs.sqlite" #"sqlite:////home/mark/Development/Datasets/discogs_dashboard/discogs.sqlite"
+SQLALCHEMY_DATABASE_URL = "sqlite:////data/discogs.sqlite" # "sqlite:////home/mark/Development/Datasets/discogs_dashboard/discogs.sqlite" #
 
 engine = _sql.create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
