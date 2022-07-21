fbad7c620575167d1cd375a7b456405e03a16a77
Mark Zwart <mark.zwart@pobox.com>
2022-07-21 23:19:40 +0200

    Remove dead code, add qty_collection_items

--- loading/igraph_test.py
+++ loading/igraph_test.py
@@ -29,7 +29,7 @@ class Database(_db_utils.DBStorage):
     def __get_artist_graph(self) -> ig.Graph:
         artists = _db_reader.Artists(db_file=self.db_file)
         df_edges = artists.edges()
-        df_vertices = artists.vertices()[['id_artist', 'name_artist', 'in_collection']]
+        df_vertices = artists.vertices()[['id_artist', 'name_artist', 'in_collection', 'qty_collection_items']]
         graph = ig.Graph.DataFrame(edges=df_edges, directed=False, vertices=df_vertices)
         vtx_collection = graph.vs.select(in_collection_eq=1)
         vtx_connectors_all = []
@@ -57,6 +57,7 @@ class Database(_db_utils.DBStorage):
                 df_dendrogram = pd.DataFrame({'id_artist': component.vs['name'],
                                             'name_artist': component.vs['name_artist'],
                                             'in_collection': component.vs['in_collection'],
+                                            'qty_collection_items': component.vs['qty_collection_items'],
                                             'id_hierarchy': [0] * qty_vertices,
                                             'id_community_from': [0] * qty_vertices,
                                             'id_community': [1] * qty_vertices, #[i + 1 for i in range(qty_vertices)],
@@ -117,6 +118,7 @@ class Database(_db_utils.DBStorage):
                 lst_communities.append(pd.DataFrame({'id_artist': graph.vs['name'],
                                                     'name_artist': graph.vs['name_artist'],
                                                     'in_collection': graph.vs['in_collection'],
+                                                    'qty_collection_items': graph.vs['qty_collection_items'],
                                                     'id_hierarchy': [tree_level] * len(graph.vs),
                                                     'id_community_from': graph.vs['id_community_from'],
                                                     'id_community': community_membership,
@@ -150,106 +152,9 @@ def plot_interactive(db_file: str) -> None:
     visnet.show('graph_community.html')
 
 
-
 # Create overall graph
 db = Database(db_file=db_file)
 db.extract_artist_edges()
 db.create_clusters()
 db.extract_community_dendrogram()
 plot_interactive(db_file=db_file)
-
-
-""" idx_community_start = len(lst_graphs) + 1
-idx_component = 0
-lst_component_communities =[]
-for graph in  lst_graphs:
-    graph = graph.copy()
-    graph.vs['id_community_from'] = [idx_component] * len(graph.vs)
-    if sum(graph.vs['in_collection']) > 1:
-        df_communities_component = db.cluster_component(graph_component=graph,
-                                                        idx_community_start=idx_community_start)
-    else:
-        df_communities_component = pd.DataFrame({'id_artist': graph.vs['name'],
-                                                'name_artist': graph.vs['name_artist'],
-                                                'in_collection': graph.vs['in_collection'],
-                                                'id_hierarchy': [0] * len(graph.vs),
-                                                'id_community_from': graph.vs['id_community_from'],
-                                                'id_community': [idx_community_start] * len(graph.vs),
-                                                'eigenvalue': graph.eigenvector_centrality(directed=False)})
-        pass
-    lst_component_communities.append(df_communities_component)
-    idx_component = idx_component + 1
-    idx_community_start = max(df_communities_component['id_community']) + 1
-df_communities = pd.concat(lst_component_communities, axis=0, ignore_index=True) """
-
-
-sys.exit()
-
-# 337708
-# 9438069 1774642 13696388    8930788
-""" def cluster_artist_graph(graph: ig.Graph) -> pd.DataFrame:
-    # Queue for processing graphs, keeping track level in community tree and relationships between branches
-    lst_processing_queue = [{'graph': graph, 'tree_level': 0}]
-
-    # For each sub graph until none in the list
-    qty_graphs_queued = len(lst_processing_queue)   # Number of graphs in the community tree
-    lst_cluster_data = []                           # Data-frames with data for a processed graph
-    community_last = 0
-    while qty_graphs_queued > 0:
-        dict_queue = lst_processing_queue.pop(0)
-        graph = (dict_queue.get('graph')).simplify()  # Remove self referential and double links
-        tree_level = dict_queue.get('tree_level')
-
-        qty_collection_items = sum(graph.vs['in_collection'])
-        qty_vertices = len(graph.vs)
-        print("Tree depth: " + str(tree_level) + " - Total graph # vertices: " + str(len(graph.vs))) # TODO: Remove
-        # Only cluster if the number of vertices is higher than 15
-        if qty_collection_items > 2:
-            # Cluster
-            tic = time.perf_counter()  # TODO: Remove
-            #cluster_hierarchy = graph.community_edge_betweenness(directed=False)
-            cluster_hierarchy = graph.community_fastgreedy()
-            toc = time.perf_counter()  # TODO: Remove
-            print(f"Finished clustering in {toc - tic:0.4f} seconds")  # TODO: Remove
-            # Setting maximum and minimum of number of clusters
-            qty_clusters = 15 if cluster_hierarchy.optimal_count > 15 else cluster_hierarchy.optimal_count
-            qty_clusters = qty_clusters if qty_clusters > 2 else 2
-            # Determine clusters
-            cluster_communities = cluster_hierarchy.as_clustering(n=qty_clusters)
-            community_membership = cluster_communities.membership
-
-            qty_vertices_sub = 0  # TODO: Remove
-            communities = set(community_membership)
-            eigenvalue = []
-            for community in communities:
-                graph_sub = cluster_communities.subgraph(community)
-                graph_sub.vs['id_community_from'] = [community + (community_last + 1)] * len(graph_sub.vs)
-                print("Community " + str(community) + " has " + str(len(graph_sub.vs)) + " vertices")  # TODO: Remove
-                qty_vertices_sub = qty_vertices_sub + len(graph_sub.vs)  # TODO: Remove
-                lst_processing_queue.append({'graph': graph_sub.copy(), 'tree_level': tree_level + 1})
-                # Calculate eigenvalue per sub_graph
-                eigenvalue = eigenvalue + graph_sub.eigenvector_centrality(directed=False)
-            print("Vertices processed: " + str(qty_vertices_sub)) # TODO: Remove
-        # else:
-        #     community_membership = [0] * qty_vertices
-        #     eigenvalue = [1] * qty_vertices
-
-        # Make sure community numbers are unique
-            community_membership = [i + (community_last + 1) for i in community_membership]
-            community_last = max(community_membership)
-            df_cluster_data = pd.DataFrame({'id_artist': graph.vs['name'],
-                                            'name_artist': graph.vs['name_artist'],
-                                            'in_collection': graph.vs['in_collection'],
-                                            'id_hierarchy': [tree_level] * qty_vertices,
-                                            'id_community_from': graph.vs['id_community_from'],
-                                            'id_community': community_membership,
-                                            'eigenvalue': eigenvalue})
-            lst_cluster_data.append(df_cluster_data)
-
-        qty_graphs_queued = len(lst_processing_queue)
-        print("Graphs in queue: " + str(qty_graphs_queued))  # TODO: Remove
-
-    df_data = pd.concat(lst_cluster_data, axis=0, ignore_index=True)
-    print("Out of my depth: done!")  # TODO: Remove
-    return df_data
- """
