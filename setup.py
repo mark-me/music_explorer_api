2c52b3c5629136c093ac900455cf0ae012bd6d13
Mark Zwart <mark.zwart@pobox.com>
2025-02-10 10:06:18 +0100

    Packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='etl_templates',
+    name='music_explorer_api',
     version='0.1.2',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
